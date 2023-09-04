73b9129055d1fd55c212604d5975127befcf57b5
io92 <143916909+io92@users.noreply.github.com>
2023-09-04 18:00:07 +0100

    Add files via upload

---
+++ Chat.js
@@ -0,0 +1,37 @@
+// src/Chat.js
+import React, { useState } from "react";
+
+function Chat() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  const handleSendMessage = () => {
+    if (newMessage.trim() === "") return;
+    setMessages([...messages, newMessage]);
+    setNewMessage("");
+  };
+
+  return (
+    <div>
+      <h2>Chat Room</h2>
+      <div className="chat-messages">
+        {messages.map((message, index) => (
+          <div key={index} className="message">
+            {message}
+          </div>
+        ))}
+      </div>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
